eed40d1b7e41147562da09720abb7cf8d5a4738a
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-11-11 05:03:18 +0200

    fix: small button change

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <h1 class="ml-2">Администрация - Начало</h1>
         <a class="button is-primary is-small ml-2" href="{{ route('admin.towns.index') }}">Градове</a>
-        <a class="button is-primary is-small ml-2" href="{{ route('admin.filling_stations.index') }}">Градове</a>
+        <a class="button is-info is-small ml-2" href="{{ route('admin.filling_stations.index') }}">Бензиностанции</a>
 {{--        <a class="button is-primary is-small ml-2" href="{{ route('users.admin.index') }}">Градове</a>--}}
 
 {{--        todo: implement direct route creating - from and to town--}}
