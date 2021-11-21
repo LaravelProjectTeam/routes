928f8714f7fe26309b357681b9fcee6f3b40eb46
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-11-21 18:12:05 +0200

    add database image, change deployment script

--- scripts/deployment-script.sh
+++ scripts/deployment-script.sh
@@ -9,4 +9,6 @@ docker tag routes-bg registry.heroku.com/routes-bg/web
 docker push registry.heroku.com/routes-bg/web
 
 heroku container:release web --app routes-bg
-heroku run php artisan migrate:fresh --seed --force --app routes-bg
+
+heroku run php artisan migrate --force --app routes-bg
+#heroku run php artisan migrate:fresh --seed --force --app routes-bg
